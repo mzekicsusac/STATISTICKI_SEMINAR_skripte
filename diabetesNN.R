ecc846387c0e39bffd4b88c66ec782ae826ab230
Marijana Zekić-Sušac <mzekicsusac@users.noreply.github.com>
2019-01-17 23:58:03 +0100

    Add files via upload

--- diabetesNN.R
+++ diabetesNN.R
@@ -1,5 +1,5 @@
 rm(list=ls()) 
-setwd("C:/Rdata/STAT_SEM/")
+setwd("C:/Rdata/")
 
 data <- data.frame(read.csv("Diabetes_data_zaR.csv", header=T, na.strings=c("","NA"), stringsAsFactors=FALSE, sep=";"))
 
@@ -19,8 +19,19 @@ hist(data$Presence.of.diabetes, col="green", breaks=2, xlim=c(0,1))
 # ------------------------------------------------------------------------------------------------
 # -------------- Neuronska mreza za klasifikaciju ------------------------------------------------
 # ------------------------------------------------------------------------------------------------
-## ------------ 1. korak - podjela na poduzorke za treniranje i testiranje 
+# ----------- 1. korak - skaliranje (normalizacija) --------------------------------------------------------------
+# ----------- Skaliranjem se sve vrijednosti preraèunavaju na interval (max-min)
+# ----------- razlog skaliranja - kako NM ne bi radila s velikim rasponima vrijednosti
+
+maxs <- apply(data, 2, max) ## parametar 2 oznacava da ce funkcija apply primijeniti na stupce, a ne na retke
+mins <- apply(data, 2, min) ## parametar 2 oznacava da ce funkcija apply primijeniti na stupce, a ne na retke
+
+total_sample_scaled <- as.data.frame(scale(data, center = mins, scale = maxs - mins))
+
+
+## ------------ 2. korak - podjela na poduzorke za treniranje i testiranje 
 # ------------------------------------------------------------------------------------------------
+
 no_rows = nrow(data)
 no_rows
 no_train = round(no_rows * 0.8)
@@ -28,16 +39,16 @@ no_train
 no_test = no_rows - (no_train)
 no_test
 
-train_sample <- data[sample(1:nrow(data), no_train, replace=FALSE),]
-test_sample <- data[sample(1:nrow(data), no_test, replace=FALSE),]
+train_sample_scaled <- total_sample_scaled[sample(1:nrow(total_sample_scaled), no_train, replace=FALSE),]
+test_sample_scaled <- total_sample_scaled[sample(1:nrow(total_sample_scaled), no_test, replace=FALSE),]
 
-## ------------ 2. korak - Nedostajuæe vrijednosti -------------------------------------------------------------- 
+## ------------ 3. korak - Nedostajuæe vrijednosti -------------------------------------------------------------- 
 ## ---------- provjera ima li missing, ako nema, ispod svake var ce ispisati nulu. 
 ## ---------- Ako ima, tada treba zamijeniti nedostajuce s nekom metodom ----------------------------------------
 
 apply(data,2,function(x) sum(is.na(x)))
 
-## ----------- 3. korak - kreiranje posebnih oznaka kategorija ako se radi o multiclass problemu klasifikacije  ----------------------
+## ----------- 4. korak - kreiranje posebnih oznaka kategorija ako se radi o multiclass problemu klasifikacije  ----------------------
 ## ------------ Ako se radi o binarnom problemu, 0 i 1, tada nije potrebno ---------------------------------------
 # u dataframe-u je dodan stupac y za kategorijalnu izlaznu varijablu : y = train_sample$Presence.of.diabetes
 #train_sample$y=ifelse(train_sample$Presence.of.diabetes,"poz","neg")
@@ -45,13 +56,6 @@ apply(data,2,function(x) sum(is.na(x)))
 #train_sample$neg=train_sample$Presence.of.diabetes!=1
 
 
-## ----------- 4. korak - skaliranje (normalizacija) --------------------------------------------------------------
-maxs <- apply(data, 2, max) ## parametar 2 oznacava da ce funkcija apply primijeniti na stupce, a ne na retke
-mins <- apply(data, 2, min) ## parametar 2 oznacava da ce funkcija apply primijeniti na stupce, a ne na retke
-
-train_sample_scaled <- as.data.frame(scale(train_sample, center = mins, scale = maxs - mins))
-test_sample_scaled <- as.data.frame(scale(test_sample, center = mins, scale = maxs - mins))
-total_sample_scaled <- as.data.frame(scale(data, center = mins, scale = maxs - mins))
 
 ## ------------ 5. korak - strukturiranje jednadžbe neuronske mreže ---------------------------------------
 
@@ -66,13 +70,14 @@ f
 ## ------------ 6. korak - pokretanje neuronske mreže ---------------------------------------
 
 # load libs
+install.packages("neuralnet")
 library(neuralnet)
 library(nnet)
 # --------- mreza 1 - sa logistic funkcijom -------------------------------------------------
-set.seed(10)
+set.seed(5)
 net1 <- neuralnet(f, train_sample_scaled, 
                          hidden=3, 
-                        learningrate=0.1,
+                        learningrate=0.01,
                         stepmax = 1e+05,
                         algorithm = "rprop+",
                         lifesign="full",
@@ -99,13 +104,14 @@ predict <- compute(net1, test_sample_scaled[,1:8])
 predict.values = 1*(predict$net.result>0.5)
 predict.values
 # prints real output values of test subsample
-test_sample$Presence.of.diabetes
-conf.mat.test=table(test_sample$Presence.of.diabetes,predict.values[,1])
+test_sample_scaled$Presence.of.diabetes
+conf.mat.test=table(test_sample_scaled$Presence.of.diabetes,predict.values[,1])
 # computes hit rate on test set
 conf.mat.test
 net1.test.hit_rate=sum(diag(conf.mat.test))/sum(conf.mat.test)
 net1.test.hit_rate
 # write out results of test prediction in file
-data_real_predicted <- data.frame(test_sample$Presence.of.diabetes, predict.values[,1])
+data_real_predicted <- data.frame(test_sample_scaled$Presence.of.diabetes, predict.values[,1])
+colnames(data_real_predicted)<-c("Real", "Predicted")
 write.table(data_real_predicted,file="net1_test_pred_values.csv",sep=";")
 
